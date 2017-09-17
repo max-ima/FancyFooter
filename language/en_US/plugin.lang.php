57468bc9f3980879b8e8de2ff5c7bd60f72af3a7
Collin Craige <collincraige@gmail.com>
2017-09-17 10:55:09 -0500

    added language file for us english

---
+++ language/en_US/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fancy Footer'] = 'Fancy Footer';
+$lang['Logo Configuration'] = 'Logo Configuration';
+$lang['Logo Image:'] = 'Logo Image:';
+$lang['Social Media'] = 'Social Media';
+$lang['Facebook:'] = 'Facebook:';
+$lang['Instagram:'] = 'Instagram:';
+$lang['Google +:'] = 'Google +:';
+$lang['YouTube:'] = 'YouTube:';
+$lang['Company Address'] = 'Company Address';
+$lang['Company Name:'] = 'Company Name:';
+$lang['Address:'] = 'Address:';
+$lang['City, State, Zip:'] = 'City, State, Zip:';
+$lang['Business Hours:'] = 'Business Hours:';
+$lang['About:'] = 'About:';
+$lang['Save'] = 'Save';
