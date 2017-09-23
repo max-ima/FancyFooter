07f01ec1d55d4d731af38bcb848a6db1953e00b7
Collin Craige <collincraige@gmail.com>
2017-09-23 13:38:29 -0500

    added Indonesia (Bahasa) translations

---
+++ language/id_ID/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fancy Footer'] = 'Fancy Footer';
+$lang['Logo Configuration'] = ' Konfigurmasi Logo';
+$lang['Logo Image:'] = 'Gambar Logo:';
+$lang['Social Media'] = 'Media Sosial';
+$lang['Facebook:'] = 'Facebook:';
+$lang['Instagram:'] = 'Instagram:';
+$lang['Google +:'] = 'Google +:';
+$lang['YouTube:'] = 'YouTube:';
+$lang['Company Address'] = 'Alamat Perusahaan';
+$lang['Company Name:'] = 'Nama Perusahaan:';
+$lang['Address:'] = 'Alamat:';
+$lang['City, State, Zip:'] = 'Kota, Provinsi, Kode Pos:';
+$lang['Business Hours:'] = 'Jam Kerja:';
+$lang['About:'] = 'Tentang kami:';
+$lang['About'] = 'Tentang kami';
+$lang['Contact'] = 'Kontak';
+$lang['Save'] = 'Simpan';
