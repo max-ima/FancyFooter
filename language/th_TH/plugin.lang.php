12eb67714ef771f0de735b7ed8798a170016c7e8
Collin Craige <collincraige@gmail.com>
2017-09-24 12:50:23 -0500

    added support for thai language

---
+++ language/th_TH/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fancy Footer'] = 'Fancy Footer';
+$lang['Logo Configuration'] = 'การกำหนดค่าโลโก้';
+$lang['Logo Image:'] = 'ภาพโลโก้:';
+$lang['Social Media'] = 'สื่อสังคม';
+$lang['Facebook:'] = 'เฟซบุค:';
+$lang['Instagram:'] = 'อินสตาแกรม:';
+$lang['Google +:'] = 'กูเกิ้ลพลัส:';
+$lang['YouTube:'] = 'ยูทูป:';
+$lang['Company Address'] = 'ที่อยู่บริษัท';
+$lang['Company Name:'] = 'ชื่อบริษัท:';
+$lang['Address:'] = ' ที่อยู่:';
+$lang['City, State, Zip:'] = 'เมือง, รัฐ, รหัสไปรษณีย์:';
+$lang['Business Hours:'] = ' เวลาทำการ:';
+$lang['About:'] = 'เกี่ยวกับ:';
+$lang['About'] = 'เกี่ยวกับ';
+$lang['Contact'] = 'ช่องทางการติดต่อ';
+$lang['Save'] = 'บันทึกข้อมูล';
+$lang['Full url to your social media profiles'] = 'ยูอาร์แอลฉบับเต็มของโซเชียลมีเดียโพรไฟล์ของท่าน';
+$lang['Telephone:'] = '電話';
