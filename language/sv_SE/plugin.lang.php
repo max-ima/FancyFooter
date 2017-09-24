9fd23ef7918d97976ff3a066a94e0753374f5fc8
Collin Craige <collincraige@gmail.com>
2017-09-24 12:59:58 -0500

    added suport for swedish/svenska language

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fancy Footer'] = 'Fancy Footer';
+$lang['Logo Configuration'] = 'Bildskonfiguration';
+$lang['Logo Image:'] = 'Logos Bild:';
+$lang['Social Media'] = 'Social Media';
+$lang['Facebook:'] = 'Facebook:';
+$lang['Instagram:'] = 'Instagram:';
+$lang['Google +:'] = 'Google+:';
+$lang['YouTube:'] = 'YouTube:';
+$lang['Company Address'] = 'Företags Adress';
+$lang['Company Name:'] = 'Företagsnamn:';
+$lang['Address:'] = 'Adress:';
+$lang['City, State, Zip:'] = 'Postadress:';
+$lang['Business Hours:'] = 'Öppningtider:';
+$lang['About:'] = 'Om:';
+$lang['About'] = 'Om';
+$lang['Contact'] = 'Kontakt';
+$lang['Save'] = 'Spara';
+$lang['Full url to your social media profiles'] = 'fullständiga social media profiler';
+$lang['Telephone:'] = 'Telefon';
