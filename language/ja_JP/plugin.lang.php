5899d38aabfd8ac433cc1cee64e9106534a001cc
Collin Craige <collincraige@gmail.com>
2017-09-24 12:35:14 -0500

    added Japanese language support

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fancy Footer'] = 'Fancy Footer';
+$lang['Logo Configuration'] = 'ロゴの設定';
+$lang['Logo Image:'] = 'ロゴデザイン:';
+$lang['Social Media'] = 'ソーシャルメディア';
+$lang['Facebook:'] = 'フェースブック:';
+$lang['Instagram:'] = 'インスタグラム:';
+$lang['Google +:'] = 'グーグルプラス:';
+$lang['YouTube:'] = 'ユーチューブ:';
+$lang['Company Address'] = '本店所在地';
+$lang['Company Name:'] = '社名:';
+$lang['Address:'] = '所在地:';
+$lang['City, State, Zip:'] = '県、市、郵便番号:';
+$lang['Business Hours:'] = '営業時間:';
+$lang['About:'] = '会社概要:';
+$lang['About'] = '关于我们';
+$lang['Contact'] = '連絡先';
+$lang['Save'] = '保存';
+$lang['Full url to your social media profiles'] = '共有';
+$lang['Telephone:'] = '電話';
