a3078f4b2a46beebeb4581c3879c1f4cdad3b0b1
Collin Craige <collincraige@gmail.com>
2017-09-24 13:05:36 -0500

    added support for khvenska language

---
+++ language/km_KH/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fancy Footer'] = 'Fancy Footer';
+$lang['Logo Configuration'] = 'កំណែស្លាកសញ្ញា';
+$lang['Logo Image:'] = 'រូបស្លាកសញ្ញា:';
+$lang['Social Media'] = 'បណ្តាញសង្គម';
+$lang['Facebook:'] = 'ហ្វេសប៊ុក:';
+$lang['Instagram:'] = 'បណ្តាញអុីងស្តាហ្គ្រាម:';
+$lang['Google +:'] = 'បណ្តាញហ្គេាកហ្គលផ្លឹស:';
+$lang['YouTube:'] = 'បណ្តាញយូធូប:';
+$lang['Company Address'] = 'អាស័យដ្ឋាន';
+$lang['Company Name:'] = 'ឈ្មេាះក្រុមហ៊ុន:';
+$lang['Address:'] = 'អាស័យដ្ឋាន:';
+$lang['City, State, Zip:'] = 'ទីក្រុង រដ្ឋ លេខកូដប្រៃសនីយ៍:';
+$lang['Business Hours:'] = 'មោ៉ងធ្វេីការ:';
+$lang['About:'] = 'ប្រវត្តិ:';
+$lang['About'] = 'ប្រវត្តិ';
+$lang['Contact'] = 'ទំនាក់ទំនង';
+$lang['Save'] = 'រក្សាទុក';
+$lang['Full url to your social media profiles'] = 'url ភ្ជាប់ទៅកាន់បណ្តាញសង្គមរបស់អ្នក';
+$lang['Telephone:'] = 'ទូរស័ព្ទ';
