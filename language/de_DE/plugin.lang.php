2a559804c6471a5a448e441faaf899d5889785b2
Collin Craige <collincraige@gmail.com>
2017-09-25 11:37:42 -0500

    added support for deutsch

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fancy Footer'] = 'Fancy Footer';
+$lang['Logo Configuration'] = 'Logo-Einstellungen';
+$lang['Logo Image:'] = 'Logo-Bild:';
+$lang['Social Media'] = 'Soziale Medien';
+$lang['Facebook:'] = 'Facebook:';
+$lang['Instagram:'] = 'Instagram:';
+$lang['Google +:'] = 'Google+:';
+$lang['YouTube:'] = 'YouTube:';
+$lang['Company Address'] = 'Firmenadresse';
+$lang['Company Name:'] = 'Firmenname:';
+$lang['Address:'] = 'Adresse:';
+$lang['City, State, Zip:'] = 'Stadt, Land, Postleitzahl:';
+$lang['Business Hours:'] = 'Öffnungszeiten:';
+$lang['About:'] = 'Über uns:';
+$lang['About'] = 'Über uns';
+$lang['Contact'] = 'Kontakt';
+$lang['Save'] = 'Speichern';
+$lang['Full url to your social media profiles'] = 'Link zu Ihrer persönlichen Homepage';
+$lang['Telephone:'] = 'Telefon';
